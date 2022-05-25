ff03b9ec6c2af7da59598e9e41233579b79fae3c
Pol Marc <100007474.joan23@fje.edu>
2022-05-25 18:21:00 +0200

    first commit

--- tienda/view/camisetas.php
+++ tienda/view/camisetas.php
@@ -97,10 +97,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!--Link para que las cartas funcionen!-->
         <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-        <!-- Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <!-- Font Awesome -->
-        <link href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" rel="stylesheet" >
         <link rel="stylesheet" href="../css/styles.css">
         <!--Link para que las cartas funcionen!-->
         <script src="../js/cartas.js"></script>
